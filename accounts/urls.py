57d871f61eab0733163e8a7a26869f1814bdb26f
Ehsan Ehsan <ehman14.king@gmail.com>
2024-03-02 10:45:27 -0800

    added seventh ders

--- accounts/urls.py
+++ accounts/urls.py
@@ -61,6 +61,7 @@ urlpatterns = [
     path('mengutech_second_third/', views.second_third, name="second_third"),
     path('mengutech_second_fourth/', views.second_fourth, name="second_fourth"),
     path('mengutech_second_fifth/', views.second_fifth, name="second_fifth"),
+    path('mengutech_second_seventh/', views.second_seventh, name="second_seventh"),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
