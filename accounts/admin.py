301de8af41173708ad65160aef473ed06a3387d9
Ehsan Ehsan <ehman14.king@gmail.com>
2024-11-03 11:02:03 -0800

    removed comments

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,30 +4,3 @@ from django.contrib import admin
 
 from .models import *
 
- 
-admin.site.register(comment)
-
-admin.site.register(comment1)
-admin.site.register(comment2)
- 
- 
-admin.site.register(comment3)
-admin.site.register(comment4)
-admin.site.register(comment5)
-admin.site.register(comment6)
-admin.site.register(comment7)
-admin.site.register(comment8)
-admin.site.register(comment9)
-admin.site.register(comment10)
-admin.site.register(comment11)
-admin.site.register(comment12)
-admin.site.register(comment13)
-admin.site.register(comment14)
-admin.site.register(comment15)
-admin.site.register(comment16)
-admin.site.register(comment17)
-admin.site.register(comment18)
-admin.site.register(comment19)
-admin.site.register(comment20)
-admin.site.register(comment21)
-admin.site.register(comment22)
--- accounts/views.py
+++ accounts/views.py
@@ -100,21 +100,8 @@ def second_therteenth(request):
     return render(request, 'accounts/mengutech_second/second_therteenth.html', context)
 
 def second_first(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-        #change the comment number below and in comment_list  
-        en = comment13(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment13.objects.all()
     #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/mengutech_second/second_first.html', {'Comment_list':Comment_list})
-# end of static pages 
-
-
+    return render(request, 'accounts/mengutech_second/second_first.html', {})
 
 #dynamic pages
 
@@ -139,300 +126,68 @@ def home(request):
  # the intro and lesson pages are here , copy this to add a new 'lesson' page
  
  
-def introduction(request):
-    
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-        
-        en = comment(name=name,comment_body=body,email=email,city=city)
-        en.save()
-        messages.success(request, "Comment added")
-         
-    
-    Comment_list= comment.objects.all()
-    
-     
-    return render(request, 'accounts/blog-single.html', {'Comment_list':Comment_list}) 
-
-
- 
-
+def introduction(request):     
+    return render(request, 'accounts/blog-single.html', {}) 
 
 def firstclass(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-        
-        en = comment1(name=name,comment_body=body,email=email,city=city)
-        en.save()
-         
-    
-    Comment_list= comment1.objects.all()
-    return render(request, 'accounts/firstclass.html', {'Comment_list':Comment_list})
-
-
-
+    return render(request, 'accounts/firstclass.html', {})
 
 def secondclass(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-        
-        en = comment2(name=name,comment_body=body,email=email,city=city)
-        en.save()
-         
-    
-    Comment_list= comment2.objects.all()
-    return render(request, 'accounts/secondclass.html',  {'Comment_list':Comment_list})
-
+    return render(request, 'accounts/secondclass.html',  {})
 
 def thirdclass(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment3(name=name,comment_body=body,email=email,city=city)
-        en.save()
-         
-    
-    Comment_list= comment3.objects.all()
-    return render(request, 'accounts/thirdclass.html',  {'Comment_list':Comment_list})
-
+    return render(request, 'accounts/thirdclass.html',  {})
 
 def fourthclass(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment4(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment4.objects.all()
-    return render(request, 'accounts/fourthclass.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/fourthclass.html',  {})
 
 def contact(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment5(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment5.objects.all()
-    return render(request, 'accounts/contact.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/contact.html',  {})
 
 def birinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment6(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment6.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/birinji-ders.html',  {'Comment_list':Comment_list})    
+    return render(request, 'accounts/birinji-ders.html',  {})    
 
 def ikkinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment7(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment7.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/ikkinji-ders.html',  {'Comment_list':Comment_list})    
+    return render(request, 'accounts/ikkinji-ders.html',  {})    
 
 def uchinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment8(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment8.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/uchinji-ders.html',  {'Comment_list':Comment_list})    
+    return render(request, 'accounts/uchinji-ders.html',  {})    
 
 def totinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment9(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment9.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/totinji-ders.html',  {'Comment_list':Comment_list})    
+    return render(request, 'accounts/totinji-ders.html',  {})    
 
 def bashinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment10(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment10.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/bashinji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/bashinji-ders.html',  {})
 
 def altinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment11(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment11.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/altinji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/altinji-ders.html',  {})
 
 def yettinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment12(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment12.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/yettinji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/yettinji-ders.html',  {})
 
-def sekkizinjiders(request):
-   
+def sekkizinjiders(request):   
     return render(request, 'accounts/sekkizinji-ders.html',  {})
 
 def toqquzinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment14(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment14.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/toqquzinji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/toqquzinji-ders.html',  {})
 
 def oninjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment15(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment15.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/oninji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/oninji-ders.html',  {})
 
 def onbirinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment16(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment16.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/onbirinji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/onbirinji-ders.html',  {})
 
 def onikkinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment17(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment17.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/onikkinji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/onikkinji-ders.html',  {})
 
 def onuchinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment18(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment18.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/onuchinji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/onuchinji-ders.html',  {})
 
 def ontotinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment19(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment19.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/ontotinji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/ontotinji-ders.html',  {})
 
 def onbashinjiders(request):
-    if request.method == "POST":
-        name= request.POST.get('name')
-        email= request.POST.get('email')
-        city= request.POST.get('city')
-        body= request.POST.get('body')
-      #change the comment number below and in comment_list  
-        en = comment20(name=name,comment_body=body,email=email,city=city)
-        en.save()
-             
-    Comment_list= comment20.objects.all()
-    #pdf = get_object_or_404(MyModel, pk=pdf_id)
-    return render(request, 'accounts/onbashinji-ders.html',  {'Comment_list':Comment_list})
+    return render(request, 'accounts/onbashinji-ders.html',  {})
 
 def onaltinjiders(request):
     return render(request, 'accounts/onaltinji-ders.html',  {})
